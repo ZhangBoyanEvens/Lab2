8f9fb98a933462ef2a9d300c8d912c3a89da16d5
Boyan <Boyan.24@ichat.sp.edu.sg>
2025-10-27 15:46:56 +0800

    Add BMI calculation and classification

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,18 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    # Calculate BMI
+    bmi = weight / (height * height)
+    print("Your BMI is: " + str(round(bmi, 2)))
+
+    # Classify BMI result
+    if bmi < 18.5:
+        print("Weight classification: Under Weight")
+    elif bmi <= 25.0:
+        print("Weight classification: Normal Weight")
+    else:
+        print("Weight classification: Over Weight")
+
+# Run the function
+calculate_bmi(weight=57, height=1.73)
